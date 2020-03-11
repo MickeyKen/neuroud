9743bf07963ccc02c34f4f196d7d943c6202fc97
Kenta Miki <mickey333ff@gmail.com>
2020-03-11 11:50:19 +0900

    [Add] original q_learning proigram

---
+++ neuroud2/scripts/my_qlearn.py
@@ -0,0 +1,42 @@
+import random
+
+class MyQLearn:
+    def __init__(self, actions, epsilon, alpha, gamma):
+        self.q = {}
+        self.epsilon = epsilon  # exploration constant
+        self.alpha = alpha      # discount constant
+        self.gamma = gamma      # discount factor
+        self.actions = actions
+
+    def getQ(self, state, action):
+        return self.q.get((state, action), 0.0)
+
+    def learnQ(self, state, action, reward, value):
+        '''
+        Q-learning:
+            Q(s, a) += alpha * (reward(s,a) + max(Q(s') - Q(s,a))
+        '''
+        oldv = self.q.get((state, action), None)
+        if oldv is None:
+            self.q[(state, action)] = reward
+        else:
+            self.q[(state, action)] = oldv + self.alpha * (reward-oldv)
+
+    def chooseAction(self, state, return_q=False):
+        q = [self.getQ(state, a) for a in self.actions]
+        maxQ = max(q)
+
+        if random.random() < self.epsilon:
+            action = int(random.uniform(0, 3))
+        else:
+            i = q.index(maxQ)
+            action = self.actions[i]
+
+        # action = self.actions[i]
+        if return_q: # if they want it, give it!
+            return action, q
+        return action
+
+    def learn(self, state1, action1, reward, state2):
+        maxqnew = max([self.getQ(state2, a) for a in self.actions])
+        self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
