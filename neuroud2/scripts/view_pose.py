c3e1c0de6732d888ae49b05400dbdda74ac4ad87
MickeyKen <mickey333ff@gmail.com>
2020-04-20 20:19:47 +0900

    [Fix] adding plt.clf()

--- neuroud2/scripts/view_pose.py
+++ neuroud2/scripts/view_pose.py
@@ -25,28 +25,35 @@ if __name__ == '__main__':
     human_number = 5
 
     name_list = []
-    name_list.append("ubiquitous_display")
+    # name_list.append("ubiquitous_display")
 
     for i in range(human_number):
         name_list.append("actor" + str(i))
 
     plt.ion()
     plt.title('HEATMAP')
-    plt.xlim(-500,500)
-    plt.ylim(-500,500)
-    plt.grid()
 
     rate = rospy.Rate(5)
 
     while not rospy.is_shutdown():
+        plt.xlim(-500,500)
+        plt.ylim(-500,500)
+        plt.grid()
+
+        pose = get_pose("ubiquitous_display")
+        x = pose.position.x*100
+        y = pose.position.y*100
+        plt.plot(int(y),int(x), '^', color="red")
 
         for n in name_list:
             pose = get_pose(n)
             x = pose.position.x*100
             y = pose.position.y*100
+            plt.plot(int(y),int(x), 'o', color="blue")
+
 
             # print (pose)
-        plt.plot(int(y),int(x), 'o', color="blue")
         plt.draw()
-        plt.pause(0.1)
+        plt.pause(0.01)
+        plt.clf()
         rate.sleep()
